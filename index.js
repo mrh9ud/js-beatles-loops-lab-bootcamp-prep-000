b2a27c225868748f23467a1915ce84affd51ce36
Matthew Heavner <mrh9ud@virginia.edu>
2019-07-02 00:40:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ function theBeatlesPlay(musicians, instruments) {
   return array
 }
 
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
 function johnLennonFacts(facts) {
-  while (facts[i] < facts.length - 1) {
-    facts.concat("!!!")
-  }
-  return facts
+  var i = 0
+  while()
 }
