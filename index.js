df4ff397526ed2ee28174dd26d46a1eb789540d7
Matthew Heavner <mrh9ud@virginia.edu>
2019-07-02 00:33:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (let i = 0; i <= musicians.length - 1; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  while (i = 0; i <= facts.length - 1; i++) {
+    facts.push(`${facts}!!!`)
+  }
+  return facts
+}
