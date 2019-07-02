ffc153d658e9e696539079273cbcdced666c28fb
Matthew Heavner <mrh9ud@virginia.edu>
2019-07-02 00:55:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,9 +34,7 @@ function iLoveTheBeatles(number) {
   var array = []
   do {
     array.push("I love the Beatles!");
-  } while (number < 15 && i >= 0) {
-    array.push(15 - number[i]);
-    i++;
+    
   }
   return array
 }
